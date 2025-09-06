e8627af2138a2ec6c03abe2314fddf7c1e879767
Le Hoang Lam <lhlam2515@gmail.com>
2025-09-06 08:58:21 +0700

    Refactor click event handler to use updateActivityCard function for better code organization

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,30 @@
 const timeframeSelector = document.getElementById("timeframe-selector");
 
-timeframeSelector.addEventListener("click", async ({ target }) => {
-  if (!target.classList.contains("timeframe-button")) return;
+const updateActivityCard = (title, timeframes, target) => {
+  const activityCard = document.getElementById(
+    title.toLowerCase().replace(" ", "-")
+  );
+  const currentHours = activityCard.querySelector(".current-hours");
+  const previousHours = activityCard.querySelector(".previous-hours");
 
-  const data = await fetch("./data.json").then((res) => res.json());
   const previousLabels = {
     daily: "Yesterday",
     weekly: "Last Week",
     monthly: "Last Month",
   };
 
-  data.forEach(({ title, timeframes }) => {
-    const activityCard = document.getElementById(
-      title.toLowerCase().replace(" ", "-")
-    );
-    const currentHours = activityCard.querySelector(".current-hours");
-    const previousHours = activityCard.querySelector(".previous-hours");
+  currentHours.textContent = `${timeframes[target].current}hrs`;
+  previousHours.textContent = `${previousLabels[target]} - ${timeframes[target].previous}hrs`;
+};
 
-    currentHours.textContent = `${timeframes[target.id].current}hrs`;
-    previousHours.textContent = `${previousLabels[target.id]} - ${
-      timeframes[target.id].previous
-    }hrs`;
-  });
+timeframeSelector.addEventListener("click", async ({ target }) => {
+  if (!target.classList.contains("timeframe-button")) return;
+
+  const data = await fetch("./data.json").then((res) => res.json());
+
+  data.forEach(({ title, timeframes }) =>
+    updateActivityCard(title, timeframes, target.id)
+  );
 });
 
 timeframeSelector.addEventListener("click", ({ target, currentTarget }) => {
